9af1ce7cc741088048013bd242617b60ab8b6893
Marian Dobre <marian.dobre@rinf.ro>
2015-09-07 19:13:12 +0300

    integrate Doctrine ORM with CodeIgniter

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -5,7 +5,27 @@ class Test extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        $this->load->library('doctrine');
+
+        var_dump($this->doctrine->em);
+
+
+//        $group = new Entity\UserGroup;
+//        $group->setName('Users');
+//        $user = new Entity\User;
+//        $user->setUsername('wildlyinaccurate');
+//        $user->setPassword('Passw0rd');
+//        $user->setEmail('wildlyinaccurate@gmail.com');
+//        $user->setGroup($group);
+        // When you have set up your database, you can persist these entities:
+        // $em = $this->doctrine->em;
+        // $em->persist($group);
+        // $em->persist($user);
+        // $em->flush();
+        $this->load->view('welcome_message', array(
+//            'user' => $user,
+//            'group' => $group,
+        ));
 	}
 
 
---
+++ application/libraries/my_composer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class MY_Composer
+ */
+class MY_Composer
+{
+    function __construct()
+    {
+        include("./vendor/autoload.php");
+    }
+}
